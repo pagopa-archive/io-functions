7ea7ed334110f680fc8fc265c6b6308518620c25
Federico Feroldi <pix@yahoo.it>
2017-08-01 11:04:14 +0200

    Endpoint returns request

--- UsersApi/index.ts
+++ UsersApi/index.ts
@@ -3,7 +3,7 @@ import * as express from "express";
 
 // Create express app as usual
 const app = express();
-app.get("/api/:foo", (req, res) => res.json({ foo: req.params.foo }));
+app.get("/api/:foo", (req, res) => res.json(req));
 app.get("/api/:foo/:bar", (req, res) => res.json({ foo: req.params.foo, bar: req.params.bar }));
 
 // Binds the express app to an Azure Function handler
