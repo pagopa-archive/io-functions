cd804c8b959830620eadc7872e705ab0e2f5d89e
Federico Feroldi <pix@yahoo.it>
2017-08-01 09:53:50 +0200

    Playing with express

--- UsersApi/index.ts
+++ UsersApi/index.ts
@@ -1,2 +1,10 @@
-import { usersApi } from "./usersApi";
-module.exports = usersApi;
+import { createAzureFunctionHandler } from "azure-function-express";
+import * as express from "express";
+
+// Create express app as usual
+const app = express();
+app.get("/api/:foo", (req, res) => res.json({ foo: req.params.foo }));
+app.get("/api/:foo/:bar", (req, res) => res.json({ foo: req.params.foo, bar: req.params.bar }));
+
+// Binds the express app to an Azure Function handler
+module.exports = createAzureFunctionHandler(app);
