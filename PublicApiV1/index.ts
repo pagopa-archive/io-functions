d55cad85650bfdffc9a085df4f29530d8dc5c322
Federico Feroldi <pix@yahoo.it>
2017-08-01 18:16:32 +0200

    Renames UsersApi to PublicApiV1

--- PublicApiV1/index.ts
+++ PublicApiV1/index.ts
@@ -1,15 +1,19 @@
+/**
+ * Main entrypoint for the public APIs handlers
+ */
+
 import { createAzureFunctionHandler } from "azure-function-express";
 import * as express from "express";
 
 const app = express();
 
-app.get("/api/debug", (req, res) =>
+app.get("/api/v1/debug", (req, res) =>
   res.json({
     env: process.env,
     headers: req.headers,
   }));
 
-app.get("/api/:foo/:bar", (req, res) => res.json({ foo: req.params.foo, bar: req.params.bar }));
+app.get("/api/v1/:foo/:bar", (req, res) => res.json({ foo: req.params.foo, bar: req.params.bar }));
 
 // Binds the express app to an Azure Function handler
 module.exports = createAzureFunctionHandler(app);
