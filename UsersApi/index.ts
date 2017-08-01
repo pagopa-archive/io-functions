727f936de8c9930c388551e5cd914612df5d8be9
Federico Feroldi <pix@yahoo.it>
2017-08-01 14:07:18 +0200

    Restoring function auth

--- UsersApi/index.ts
+++ UsersApi/index.ts
@@ -1,11 +1,15 @@
 import { createAzureFunctionHandler } from "azure-function-express";
 import * as express from "express";
 
-// Create express app as usual
 const app = express();
-app.get("/api/:foo", (req, res) => res.json({
-  ok: "OK",
-}));
+
+app.get("/api/debug", (req, res) => {
+  res.json({
+    env: process.env,
+    headers: req.headers,
+  });
+});
+
 app.get("/api/:foo/:bar", (req, res) => res.json({ foo: req.params.foo, bar: req.params.bar }));
 
 // Binds the express app to an Azure Function handler
