8f702677e8203523c7e700e1977fe4f71fc0176d
Federico Feroldi <federico@teamdigitale.governo.it>
2017-08-04 10:18:52 +0200

    Adds back users handlers

--- PublicApiV1/index.ts
+++ PublicApiV1/index.ts
@@ -7,14 +7,11 @@ import * as express from "express";
 
 const app = express();
 
-/*
 import * as mongoose from "mongoose";
 import { IUser } from "./interfaces/user";
 import { IUserModel } from "./models/user";
 import { userSchema } from "./schemas/user";
-*/
 
-/*
 const MONGODB_CONNECTION: string = process.env.CUSTOMCONNSTR_mongo;
 const connection: mongoose.Connection = mongoose.createConnection(
   MONGODB_CONNECTION,
@@ -25,7 +22,6 @@ const connection: mongoose.Connection = mongoose.createConnection(
   },
 );
 const User = connection.model<IUserModel>("User", userSchema);
-*/
 
 import debugHandler from "./debugHandler";
 app.get("/api/v1/debug", (req, res) => res.json({
@@ -33,11 +29,9 @@ app.get("/api/v1/debug", (req, res) => res.json({
     headers: req.headers,
   }));
 
-/*
 import { getUserHandler, updateUserHandler } from "./usersApi";
 app.get("/api/v1/users/:fiscalcode", getUserHandler);
 app.post("/api/v1/users/:fiscalcode", updateUserHandler);
-*/
 
 // Binds the express app to an Azure Function handler
 module.exports = createAzureFunctionHandler(app);
