f0abf00daf3d983f7f516b3a4b76c0bc31342e07
Federico Feroldi <federico@teamdigitale.governo.it>
2017-08-14 16:11:36 +0200

    Adds types to context

--- CreatedMessageQueueHandler/index.ts
+++ CreatedMessageQueueHandler/index.ts
@@ -1,11 +1,54 @@
-export function index(context: any) {
-  context.log("Queue trigger function processed work item", context.bindings.createdMessage);
-  context.log("queueTrigger =", context.bindingData.queueTrigger);
-  context.log("expirationTime =", context.bindingData.expirationTime);
-  context.log("insertionTime =", context.bindingData.insertionTime);
-  context.log("nextVisibleTime =", context.bindingData.nextVisibleTime);
-  context.log("id=", context.bindingData.id);
-  context.log("popReceipt =", context.bindingData.popReceipt);
-  context.log("dequeueCount =", context.bindingData.dequeueCount);
-  context.done();
+interface IBindingData {
+  queueTrigger?: string;
+  expirationTime?: Date;
+  insertionTime?: Date;
+  nextVisibleTime?: Date;
+  id: string;
+  popReceipt: string;
+  dequeueCount: number;
 }
+
+interface IContext {
+  bindingData: {
+    queueTrigger?: string;
+    expirationTime?: Date;
+    insertionTime?: Date;
+    nextVisibleTime?: Date;
+    id: string;
+    popReceipt: string;
+    dequeueCount: number;
+  };
+  log: (msg: any, params?: any) => any;
+  done: () => void;
+}
+
+interface IContextWithBindings extends IContext {
+  bindings: {
+    createdMessage?: string;
+  };
+}
+
+interface IMessagePayload {
+  messageId?: string;
+}
+
+export function index(context: IContextWithBindings) {
+  if (context.bindings.createdMessage != null) {
+    const message: IMessagePayload = JSON.parse(context.bindings.createdMessage);
+    context.log(`Dequeued message [${message.messageId}]`);
+  } else {
+    context.log(`Fatal! no message found in bindings.`);
+    context.done();
+  }
+}
+
+/*
+2017-08-14T13:58:19.356 Queue trigger function processed work item { messageId: '5991ac7944430d3670b81b74' }
+2017-08-14T13:58:19.356 queueTrigger = {"messageId":"5991ac7944430d3670b81b74"}
+2017-08-14T13:58:19.356 expirationTime = 8/21/2017 1:58:17 PM +00:00
+2017-08-14T13:58:19.356 insertionTime = 8/14/2017 1:58:17 PM +00:00
+2017-08-14T13:58:19.356 nextVisibleTime = 8/14/2017 2:08:19 PM +00:00
+2017-08-14T13:58:19.356 id= 5f149158-92fa-4aaf-84c9-667750fdfaad
+2017-08-14T13:58:19.356 popReceipt = AgAAAAMAAAAAAAAAtS7dxwYV0wE=
+2017-08-14T13:58:19.356 dequeueCount = 1
+*/
