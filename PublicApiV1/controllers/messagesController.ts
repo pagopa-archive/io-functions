9c2ed365870e5d7d791d4e9d54ca2a5c7520b2ba
Federico Feroldi <federico@teamdigitale.governo.it>
2017-08-14 08:51:20 +0200

    Adds wrapper for validating fiscal codes

--- PublicApiV1/controllers/messagesController.ts
+++ PublicApiV1/controllers/messagesController.ts
@@ -1,37 +1,46 @@
 import * as express from "express";
 
-import { isFiscalCode } from "../utils/fiscalcode";
+import { FiscalCode, isFiscalCode } from "../utils/fiscalcode";
 
 import { IMessage } from "../interfaces/message";
 import { MessageModel } from "../models/message";
 
+type RequestWithFiscalCodeHandler = (req: express.Request, res: express.Response, fiscalcode: FiscalCode) => any;
+
+function withValidFiscalCode(handler: RequestWithFiscalCodeHandler): express.RequestHandler {
+  return (request: express.Request, response: express.Response, _: express.NextFunction) => {
+    const fiscalCode: string = request.params.fiscalcode;
+    if (isFiscalCode(fiscalCode)) {
+      return handler(request, response, fiscalCode);
+    } else {
+      response.status(400).send(`The fiscal code [${fiscalCode}] is not valid.`);
+    }
+  };
+}
+
 /**
- * Returns a createMessage controller
+ * Returns a createMessage controller that will handle requests
+ * for creating new messages.
  *
  * @param Message The Message model.
  */
 export function createMessageController(Message: MessageModel): express.RequestHandler {
-  return (request: express.Request, response: express.Response) => {
-    const fiscalCode: string = request.params.fiscalcode;
-    if (isFiscalCode(fiscalCode)) {
-      const message: IMessage = {
-        bodyShort: request.body.body_short,
-        fiscalCode,
-      };
-      Message.createMessage(message).then((result) => {
-        if (result != null) {
-          response.json(result);
-        } else {
-          response.status(500).send("Did not create");
-        }
-      },
-      (error) => {
-        response.status(500).json({
-          error,
-        });
+  return withValidFiscalCode((request: express.Request, response: express.Response, fiscalCode: FiscalCode) => {
+    const message: IMessage = {
+      bodyShort: request.body.body_short,
+      fiscalCode,
+    };
+    Message.createMessage(message).then((result) => {
+      if (result != null) {
+        response.json(result);
+      } else {
+        response.status(500).send("Did not create");
+      }
+    },
+    (error) => {
+      response.status(500).json({
+        error,
       });
-    } else {
-      response.status(404).send("Not found");
-    }
-  };
+    });
+  });
 }
