f8339191e1ad4e4e0c638bf58802586ba9c4079f
Federico Feroldi <federico@teamdigitale.governo.it>
2017-08-14 13:54:40 +0200

    Troubleshooting message queueing

--- PublicApiV1/controllers/messages.ts
+++ PublicApiV1/controllers/messages.ts
@@ -29,19 +29,29 @@ export function CreateMessage(
       fiscalCode,
     };
     _log(">> creating message");
+    const msg = `test-message-${Date.now()}`;
+    
+    _log(`Queing [${msg}] to [${queueName}]`);
+    queueService.createMessage(queueName, msg, {}, (error) => {
+      _log(`>> message queued [${error}]`);
+      response.json({
+        notification: error == null,
+        msg,
+      });
+    });
+    
+    /*
     Message.createMessage(message).then((result) => {
       _log(`>> message created [${result._id}]`);
-      queueService.createMessage(queueName, result._id as string, {}, (error) => {
+      queueService.createMessage(queueName, `${result._id}`, {}, (error) => {
         _log(`>> message queued [${error}]`);
-        if (error) {
-          // TODO: handle error
-        }
         response.json({
-          notification: true,
+          notification: error == null,
           result,
         });
       });
     }, handleErrorAndRespond(response));
+    */
 
   });
 }
