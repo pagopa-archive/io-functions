8a89e05276feeb7bc8fee275ec0f7aba95bdeda7
Federico Feroldi <federico@teamdigitale.governo.it>
2017-08-14 14:40:41 +0200

    Adds CreatedMessageQueueHandler function

---
+++ CreatedMessageQueueHandler/index.ts
@@ -0,0 +1,13 @@
+import { IContext } from "@ksjogo/azure-functions-typescript";
+
+export function index(context: IContext) {
+  context.log("Node.js queue trigger function processed work item", context.bindings.createdMessage);
+  context.log("queueTrigger =", context.bindingData.queueTrigger);
+  context.log("expirationTime =", context.bindingData.expirationTime);
+  context.log("insertionTime =", context.bindingData.insertionTime);
+  context.log("nextVisibleTime =", context.bindingData.nextVisibleTime);
+  context.log("id=", context.bindingData.id);
+  context.log("popReceipt =", context.bindingData.popReceipt);
+  context.log("dequeueCount =", context.bindingData.dequeueCount);
+  context.done();
+}
