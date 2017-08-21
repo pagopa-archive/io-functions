1730dd3d0acc53ac49fc9badb4401df255202e1d
Spinelli Danilo <danilo.spinelli@agid.gov.it>
2017-08-21 17:12:16 +0200

    fix(deploy): fix cleanup procedure

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -6,11 +6,11 @@ source ./variables.sh
 
 az functionapp delete -g $resourceGroupName -n $functionsName
 
-az appservice plan delete --name $appService --resource-group $resourceGroupName --sku $appServiceSku
+az appservice plan delete --name $appService --resource-group $resourceGroupName
 
 az storage account delete -y --name $storageName --resource-group $resourceGroupName
 
-az cosmosdb delete --name $cosmosname --resource-group $resourceGroupName
+az cosmosdb delete --name $cosmosName --resource-group $resourceGroupName
 
 # Alternative (better) cleanup procedure 
 #   if you have got the rights:
