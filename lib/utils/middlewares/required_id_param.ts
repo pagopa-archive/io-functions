d00d21280febe9bee8446c7c8ad6bdcfc8fac366
Federico Feroldi <federico@teamdigitale.governo.it>
2017-09-11 12:35:22 +0200

    Adds tests for RequiredIdParamMiddleware

--- lib/utils/middlewares/required_id_param.ts
+++ lib/utils/middlewares/required_id_param.ts
@@ -2,12 +2,16 @@ import { left, right } from "../either";
 
 import { RequiredParamMiddleware } from "./required_param";
 
+/**
+ * Returns an instance of RequiredParamMiddleware that extracts a non-empty "id"
+ * parametere from the request URI parameters.
+ */
 export const RequiredIdParamMiddleware = RequiredParamMiddleware<string>(
   (params) => {
     if (typeof params.id === "string" && (params.id as string).length > 0) {
       return right(params.id as string);
     } else {
-      return left("The ID parameter is required");
+      return left("The 'id' parameter is required and must be non-empty.");
     }
   },
 );
