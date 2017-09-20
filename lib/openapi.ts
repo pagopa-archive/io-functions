d149544d33c214324a0507ec91ca321ad1f9b078
Federico Feroldi <cloudify@users.noreply.github.com>
2017-09-20 08:46:49 +0200

    [#150738001] Exposes the openapi specs to the proper URI (#55)

---
+++ lib/openapi.ts
@@ -0,0 +1,33 @@
+/**
+ * Entrypoint for the Openapi handler
+ */
+
+import { IContext } from "azure-function-express";
+
+import * as express from "express";
+import * as winston from "winston";
+
+import { setAppContext } from "./utils/middlewares/context_middleware";
+
+import { configureAzureContextTransport } from "./utils/logging";
+
+import { createAzureFunctionHandler } from "azure-function-express";
+
+import { GetOpenapi } from "./controllers/openapi";
+
+import { specs as publicApiV1Specs } from "./api/public_api_v1";
+
+// Setup Express
+
+const app = express();
+
+app.get("/swagger.json", GetOpenapi(publicApiV1Specs));
+
+const azureFunctionHandler = createAzureFunctionHandler(app);
+
+// Binds the express app to an Azure Function handler
+export function index(context: IContext<{}>): void {
+  configureAzureContextTransport(context, winston, "debug");
+  setAppContext(app, context);
+  azureFunctionHandler(context);
+}
