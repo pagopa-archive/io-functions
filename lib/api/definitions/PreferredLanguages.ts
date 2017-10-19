370e0fb914d8557939ff791d10f13164f2b3b386
Spinelli Danilo <danilo.spinelli@agid.gov.it>
2017-10-19 11:18:02 +0200

    fix: regenerated models with new api

--- lib/api/definitions/PreferredLanguages.ts
+++ lib/api/definitions/PreferredLanguages.ts
@@ -7,7 +7,7 @@
 // tslint:disable:no-any
 
 /**
- * 
+ * Indicates the User's preferred written or spoken languages in order of preference. Generally used for selecting a localized User interface. Valid values are concatenation of the ISO 639-1 two letter language code, an underscore, and the ISO 3166-1 2 letter country code; e.g., 'en_US' specifies the language English and country US.
  */
 
 import { option, Option } from "ts-option";
