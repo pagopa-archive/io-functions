6f69a7c28a8ad78dcafe1ab5ba046cc28d385940
Federico Feroldi <federico@teamdigitale.governo.it>
2017-10-24 11:45:21 +0200

    [#151370067] Implements the first version of a release process
    Initial batch of work, implements version bump and prepares
    for #150764052

---
+++ lib/index.ts
@@ -0,0 +1,7 @@
+// tslint:disable-next-line:no-object-mutation
+module.exports = {
+  CreatedMessageQueueHandler: require("./created_message_queue_handler"),
+  EmailNotificationsQueueHandler: require("./emailnotifications_queue_handler"),
+  Openapi: require("./openapi"),
+  PublicApiV1: require("./public_api_v1")
+};
