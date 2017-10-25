7f43f32538c927bf563229919bdeb202cb39e3be
Federico Feroldi <federico@teamdigitale.governo.it>
2017-10-25 10:59:44 +0200

    [#150764052] Tentative fix for entry point error
    see https://github.com/Azure/azure-functions-pack/issues/39

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,4 @@
-// tslint:disable-next-line:no-object-mutation
-module.exports = {
+export = {
   CreatedMessageQueueHandler: require("./created_message_queue_handler"),
   EmailNotificationsQueueHandler: require("./emailnotifications_queue_handler"),
   Openapi: require("./openapi"),
