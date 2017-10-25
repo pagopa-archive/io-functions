f6f0ebe09219865a38a6f20a0e6a308be0f69abc
Federico Feroldi <federico@teamdigitale.governo.it>
2017-10-25 11:16:26 +0200

    [#150764052] Tentative fix for entry point error

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,7 @@
 export = {
-  CreatedMessageQueueHandler: require("./created_message_queue_handler"),
-  EmailNotificationsQueueHandler: require("./emailnotifications_queue_handler"),
-  Openapi: require("./openapi"),
-  PublicApiV1: require("./public_api_v1")
+  CreatedMessageQueueHandler: require("./created_message_queue_handler").index,
+  EmailNotificationsQueueHandler: require("./emailnotifications_queue_handler")
+    .index,
+  Openapi: require("./openapi").index,
+  PublicApiV1: require("./public_api_v1").index
 };
