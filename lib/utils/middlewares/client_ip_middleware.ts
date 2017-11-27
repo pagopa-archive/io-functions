1e4da6ce3597bba7369c8273c48510fbc315d6c4
Danilo Spinelli <gunzip@users.noreply.github.com>
2017-11-27 16:48:38 +0100

    chore: clarified ip checking middleware (#105)
    * chore: clarified ip checking middleware
    
    * fix: code review

--- lib/utils/middlewares/client_ip_middleware.ts
+++ lib/utils/middlewares/client_ip_middleware.ts
@@ -7,13 +7,22 @@ import { IRequestMiddleware } from "../request_middleware";
 
 export type ClientIp = Option<IPString>;
 
+import * as winston from "winston";
+
 /**
- * A middleware that extracts the client IP from the request
+ * A middleware that extracts the client IP from the request.
+ *
+ * The algorithm to extract the IP is documented here:
+ * https://www.npmjs.com/package/request-ip#how-it-works
+ *
+ * If you call Functions logic bypassing the API gateway
+ * the IP will be null and the middleware will return None.
  */
 export const ClientIpMiddleware: IRequestMiddleware<
   never,
   ClientIp
 > = request => {
   const clientIp = requestIp.getClientIp(request);
+  winston.debug(`Handling request for client IP|${clientIp}`);
   return Promise.resolve(right<never, ClientIp>(toIPString(clientIp)));
 };
