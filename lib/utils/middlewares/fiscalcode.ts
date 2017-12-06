9d04f909d0d6000efdd5a302039ad5b8137e78aa
Federico Feroldi <federico@teamdigitale.governo.it>
2017-12-06 18:40:11 +0100

    Converts FiscalCodeMiddleware to a RequiredParamMiddleware

--- lib/utils/middlewares/fiscalcode.ts
+++ lib/utils/middlewares/fiscalcode.ts
@@ -1,26 +1,13 @@
-import * as t from "io-ts";
-
 import { FiscalCode } from "../../api/definitions/FiscalCode";
 
-import { IRequestMiddleware } from "../request_middleware";
-import {
-  IResponseErrorValidation,
-  ResponseErrorFromValidationErrors
-} from "../response";
+import { RequiredParamMiddleware } from "./required_param";
 
 /**
  * A request middleware that validates the presence of a valid `fiscalcode` parameter
  * in the request. In case the parameter is missing or is not valid, the middleware
  * returns an `IResponseErrorValidation`.
  */
-export const FiscalCodeMiddleware: IRequestMiddleware<
-  IResponseErrorValidation,
+export const FiscalCodeMiddleware = RequiredParamMiddleware(
+  "fiscalcode",
   FiscalCode
-> = request =>
-  new Promise(resolve => {
-    const validation = t.validate(request.params.fiscalcode, FiscalCode);
-    const result = validation.mapLeft(
-      ResponseErrorFromValidationErrors(FiscalCode)
-    );
-    resolve(result);
-  });
+);
