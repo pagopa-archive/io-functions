271effb3c3f7d328783183f01c54b0cabea5073d
Federico Feroldi <federico@teamdigitale.governo.it>
2018-03-02 18:38:02 +0100

    Adds DangerJS for custom coding rules

---
+++ Dangerfile.ts
@@ -0,0 +1,6 @@
+// import custom DangerJS rules
+// see http://danger.systems/js
+// see https://github.com/teamdigitale/danger-plugin-digitalcitizenship/
+import checkDangers from 'danger-plugin-digitalcitizenship';
+
+checkDangers();
