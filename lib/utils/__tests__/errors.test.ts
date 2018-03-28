8883aafe707d4677e03529aef29c1eab2a42dbe3
Spinelli Danilo <danilo.spinelli@agid.gov.it>
2018-03-28 19:16:20 +0200

    [#155970981] - fix typo

--- lib/utils/__tests__/errors.test.ts
+++ lib/utils/__tests__/errors.test.ts
@@ -6,11 +6,11 @@ afterEach(() => {
 });
 
 describe("of", () => {
-  it("should build a RuntimError from an unknow Error", async () => {
+  it("should build a RuntimeError from an unknow Error", async () => {
     const runtimeError = of(new Error());
     expect(runtimeError.kind).toEqual("UnknowError");
   });
-  it("should build a RuntimError from a RuntimError", async () => {
+  it("should build a RuntimeError from a RuntimError", async () => {
     const runtimeError = of(TransientError(""));
     expect(runtimeError.kind).toEqual("TransientError");
   });
