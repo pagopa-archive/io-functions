d48b57cff24be9a8c36e71ec93c3b26226f1d4b1
Danilo Spinelli <gunzip@users.noreply.github.com>
2018-05-28 18:30:30 +0200

    [#157917582] return transient error in case the API proxy returns 5xx HTTP status (#243)
    * [#157917582] fix: return transient error in case of 5xx from API proxy
    
    * fix: added tests and fixed error handling
    
    * fix: avoid extra call to math.floor

--- lib/webhook_queue_handler.ts
+++ lib/webhook_queue_handler.ts
@@ -180,24 +180,28 @@ export async function sendToWebhook(
             // in case of server HTTP 5xx errors we trigger a retry
             response.serverError
               ? TransientError(
-                  `Transient HTTP error calling API Proxy API: ${response.text}`
+                  `Transient HTTP error calling API Proxy: ${response.text}`
                 )
               : PermanentError(
-                  `Permanent HTTP error calling API Proxy API: ${response.text}`
+                  `Permanent HTTP error calling API Proxy: ${response.text}`
                 )
           );
         }
         return right<RuntimeError, ApiProxyResponse>(response.body);
       },
       err => {
+        const errorMsg =
+          err.response && err.response.text
+            ? err.response.text
+            : "unknown error";
         return left<RuntimeError, ApiProxyResponse>(
           err.timeout
-            ? TransientError(`Timeout calling API Proxy API`)
-            : // Fail with a permanent error in case any exception
-              // is thrown during the HTTP request
-              PermanentError(
-                `Permanent error calling API Proxy API: ${JSON.stringify(err)}`
-              )
+            ? TransientError(`Timeout calling API Proxy`)
+            : // when the server returns an HTTP 5xx error
+              err.status && err.status % 500 < 100
+              ? TransientError(`Transient error calling API proxy: ${errorMsg}`)
+              : // when the server returns some other type of HTTP error
+                PermanentError(`Permanent error calling API Proxy: ${errorMsg}`)
         );
       }
     );
