485b26075ee5d4b9c5b5411cb2aa389f76a0e100
Spinelli Danilo <danilo.spinelli@agid.gov.it>
2018-07-05 17:01:00 +0200

    #158632515 upgrade winston

--- lib/utils/logging.ts
+++ lib/utils/logging.ts
@@ -1,37 +1,36 @@
 // tslint:disable:no-object-mutation
 
-import * as winston from "winston";
-
 import { IContext } from "azure-function-express";
+import * as logform from "logform";
+import * as winston from "winston";
+import * as Transport from "winston-transport";
+
+const { timestamp, printf } = logform.format;
 
 /**
  * A custom Winston Transport that logs to the Azure Functions context
  */
-class AzureContextTransport extends winston.Transport {
+class AzureContextTransport extends Transport {
   // tslint:disable-next-line:readonly-keyword
   private azureContext: IContext<{}>;
 
-  constructor(azureContext: IContext<{}>, options: winston.TransportOptions) {
+  constructor(
+    azureContext: IContext<{}>,
+    options: Transport.TransportStreamOptions
+  ) {
     super(options);
-
-    this.name = "AzureContextLogger";
     this.level = options.level || "info";
-
     this.azureContext = azureContext;
   }
 
   public log(
-    _: string,
     msg: string,
-    __: object | undefined,
     callback: (err: Error | undefined, cont: boolean) => void
   ): void {
     if (this.silent) {
       return callback(undefined, true);
     }
-
     this.azureContext.log(msg);
-
     callback(undefined, true);
   }
 }
@@ -41,14 +40,21 @@ class AzureContextTransport extends winston.Transport {
  */
 export function configureAzureContextTransport(
   context: IContext<{}>,
-  w: winston.Winston,
+  w: typeof winston,
   level: string
 ): void {
   const azureContextTransport = new AzureContextTransport(context, {
     level
   });
   w.configure({
-    level,
+    format: w.format.combine(
+      timestamp(),
+      w.format.splat(),
+      w.format.simple(),
+      printf(nfo => {
+        return `${nfo.timestamp} [${nfo.level}]: ${nfo.message}`;
+      })
+    ),
     transports: [azureContextTransport]
   });
 }
